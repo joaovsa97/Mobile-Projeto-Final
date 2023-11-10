116c6288c38583118e5ac4d0b5b6e5662feef60d
joaovsa97 <joaos.azevedo10@gmail.com>
2023-11-10 17:40:17 -0300

    feat: finalizada navegação por tab e ícones

--- App.js
+++ App.js
@@ -1,20 +1,67 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { Container, Title } from "./styles/GlobalStyles";
+import { IconButton, Text } from "react-native-paper";
 import { Entypo } from "@expo/vector-icons";
 import "react-native-gesture-handler";
 
-import Login from "./pages/Login"
-import Logged from "./pages/Logged"
+import Login from "./pages/Login";
+import Logged from "./pages/Logged";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator screenOptions={{
+            headerStyle: {
+              backgroundColor: '#6666CC',
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+              fontWeight: 'bold'
+            },
+          }}>
         <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="Logged" component={Logged} />
+        <Stack.Screen
+          name="Logged"
+          component={Logged}
+          options={({ navigation }) => ({
+            title: "",
+            headerLeft: () => (
+              <>
+                <IconButton
+                  icon="account"
+                  iconColor="white"
+                  size={40}
+                  onPress={() => navigation.navigate("Perfil")}
+                />
+                <Text style={{fontSize: 20, fontWeight: 'bold', color: 'white'}}>John Doe</Text>
+              </>
+            ),
+            headerRight: () => (
+              <>
+                <IconButton
+                  icon="plus"
+                  iconColor="white"
+                  onPress={() =>
+                    alert(
+                      'Função de Criar Publicação está em desenvolvimento'
+                    )
+                  }
+                />
+                <IconButton
+                  icon="message"
+                  iconColor="white"
+                  onPress={() =>
+                    alert(
+                      'Função de Chat está em desenvolvimento'
+                    )
+                  }
+                />
+              </>
+            ),
+          })}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
